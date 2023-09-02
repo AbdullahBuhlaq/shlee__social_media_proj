f260e076ee8bf73a51a992d656333881bde401bc
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2023-09-02 15:20:49 +0300

    fix socket

--- server/app.js
+++ server/app.js
@@ -46,7 +46,7 @@ app.use(upload());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
   },
 });
 app.use(cors());
